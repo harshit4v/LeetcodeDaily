ef667b2fcdeaa9c96ae2652ba3b66477fe2e4392
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2025-02-13 15:02:40 +0000

    [LeetCode Sync] Runtime - 120 ms (97.75%), Memory - 69.9 MB (100.00%)

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+       int res=0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<k) pq.add(nums[i]);
+        }
+        long x,y;
+        while(!pq.isEmpty()){
+            res++;
+            x=pq.poll();
+            if(pq.isEmpty()) break;
+            y=pq.poll();            
+            long val=(long)x*2+y;
+            if(val<k){
+                pq.add((int)val);
+            }            
+        }
+        return res;
+    }
+}
