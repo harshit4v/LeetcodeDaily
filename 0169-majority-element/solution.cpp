a0b51d50425c00619610625d77ddbddf9cf75f7f
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2025-02-14 20:00:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 28.2 MB (67.06%)

---
+++ 0169-majority-element/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int cnt=0;
+        int el=0;
+        for(int i=0;i<arr.size();i++){
+            if(cnt==0){
+                cnt=1;
+                el=arr[i];
+            }
+            else if(arr[i]==el){
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+        int cnt1=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==el){
+                cnt1++;
+            }
+        }
+        if(cnt1>(arr.size()/2)){
+            return el;
+        }
+        return -1;
+    }
+};
