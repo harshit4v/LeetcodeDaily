02114547d05faa09530646319acc37ebc46504d1
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2025-09-25 11:23:01 +0000

    [LeetCode Sync] Runtime - 2 ms (16.11%), Memory - 9.3 MB (62.11%)

---
+++ 0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    private:
+    void func(int ind,string digits,string s, vector<string>&ans,string combos[]){
+        if(ind==digits.size()){
+            ans.push_back(s);
+            return;
+        }
+        int digit=digits[ind]-'0';
+        for(int i=0;i<combos[digit].size();i++){
+            func(ind+1,digits,s+combos[digit][i],ans,combos);
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        string combos[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string s="";
+        vector<string>ans;
+        if (digits.empty()) return ans; 
+        func(0,digits,s,ans,combos);
+        return ans;
+    }
+};
