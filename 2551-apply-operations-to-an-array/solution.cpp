a643a817477758c82fd6503077b679aac05e98bc
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-01-18 11:25:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.1 MB (90.38%)

---
+++ 2551-apply-operations-to-an-array/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+                i++;
+            }
+        }
+        int j=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1)return nums;
+        for(int i=j+1;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+            j++;
+            }
+        }
+        return nums;
+    }
+};
