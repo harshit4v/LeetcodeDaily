8da01111aa12d91c8f005e4d9aad240a7c8d367d
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-01-24 19:22:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.7 MB (64.40%)

--- 0229-majority-element-ii/solution.cpp
+++ 0229-majority-element-ii/solution.cpp
@@ -2,45 +2,39 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        vector<int>result;
-        int cnt1=0,cnt2=0;
-        int el1=INT_MIN,el2=INT_MIN;
-        for(int i=0;i<n;i++){
-            if(cnt1==0 && el2!=nums[i]){
-                cnt1++;
-                el1=nums[i];
-            }
-            else if(cnt2==0 && el1!=nums[i]){
-                cnt2++;
-                el2=nums[i];
-            }
-            else if(el1==nums[i]){
-                cnt1++;
-            }
-            else if(el2==nums[i]){
-                cnt2++;
-            }
-            else{
-                cnt1--;
-                cnt2--;
-            }
+    vector<int>res;
+    int cnt1=0,cnt2=0,ele1=0,ele2=0;
+    for(int i=0;i<n;i++){
+        if(cnt1==0 && nums[i]!=ele2){
+            cnt1=1;
+            ele1=nums[i];
         }
-        cnt1=0,cnt2=0;
+        else if(cnt2==0 && nums[i]!=ele1){
+            cnt2=1;
+            ele2=nums[i];
+        }
+        else if(nums[i]==ele1)cnt1++;
+        else if(nums[i]==ele2)cnt2++;
+        else {
+            cnt1--,cnt2--;
+        }
+    }
+        int ct1=0,ct2=0;
         for(int i=0;i<n;i++){
-            if(nums[i]==el1){
-                cnt1++;
+            if(nums[i]==ele1){
+                ct1++;
             }
-            if(nums[i]==el2){
-                cnt2++;
+            if(nums[i]==ele2){
+                ct2++;
             }
         }
         int mini=n/3+1;
-        if(cnt1>=mini){
-            result.push_back(el1);
+        if(ct1>=mini){
+            res.push_back(ele1);
         }
-        if(cnt2>=mini && el1!=el2){
-            result.push_back(el2);
+        if(ct2 >=mini && ele1!=ele2){
+            res.push_back(ele2);
         }
-        return result;
+    return res;
     }
 };
