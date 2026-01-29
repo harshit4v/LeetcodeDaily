95b077c3b6a529827a7daa5133c6625d70e0be6a
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-01-29 07:43:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.2 MB (55.13%)

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int mini=INT_MAX;
+        for(int i=0;i<=nums.size()-k;i++){
+            mini=min(mini,(nums[i+k-1]-nums[i]));
+        }
+        return mini;
+    }
+};
