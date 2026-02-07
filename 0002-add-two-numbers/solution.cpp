c31cdf44d745955ee67d51860da501caad63bbb2
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-02-07 18:28:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 77.1 MB (45.21%)

--- 0002-add-two-numbers/solution.cpp
+++ 0002-add-two-numbers/solution.cpp
@@ -11,27 +11,6 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // ListNode *dummyHead=new ListNode(-1);
-        // ListNode *curr=dummyHead;
-        // ListNode *temp1=l1;
-        // ListNode *temp2=l2;
-        // int carry=0;
-        // while(temp1!=NULL || temp2!=NULL){
-        //     int sum=carry;
-        //     if(temp1)sum=sum+temp1->val;
-        //     if(temp2)sum+=temp2->val;
-        //     ListNode *newNode = new ListNode(sum%10);
-        //     carry=sum/10;
-        //     curr->next=newNode;
-        //     curr=curr->next;
-        //     if(temp1) temp1=temp1->next;
-        //     if(temp2) temp2=temp2->next;
-        // }
-        // if(carry){
-        //     ListNode *newNode=new ListNode(carry);
-        //     curr->next=newNode;
-        // }
-        // return dummyHead->next;
         ListNode *dummy=new ListNode(-1);
         ListNode *curr=dummy;
         ListNode *temp1=l1;
