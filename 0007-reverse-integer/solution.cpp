bf1b20825684705ffe723157bd2eeb6d2f83de50
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-06-23 18:19:04 +0000

    [LeetCode Sync] Runtime - 5 ms (20.06%), Memory - 8.3 MB (99.95%)

--- 0007-reverse-integer/solution.cpp
+++ 0007-reverse-integer/solution.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int reverse(int x) {
         int ans=0;
-         
         while(x!=0){
-            
             int digit=x%10;
             if(ans>(INT_MAX)/10 || ans<(INT_MIN)/10){
             return 0;
@@ -13,6 +11,5 @@ public:
             x/=10;
         }
         return ans;
-       
     }
 };
