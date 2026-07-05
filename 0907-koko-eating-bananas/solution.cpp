f47d00dff25202adc50416262713ba0cc6040575
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-05 12:04:44 +0000

    [LeetCode Sync] Runtime - 7 ms (70.85%), Memory - 22.8 MB (98.53%)

--- 0907-koko-eating-bananas/solution.cpp
+++ 0907-koko-eating-bananas/solution.cpp
@@ -1,46 +1,21 @@
 class Solution {
-    private:
-    int findMax(vector<int>&nums){
-        int maxi=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            maxi=max(maxi,nums[i]);
-        }
-        return maxi;
-    }
-    long long calculateTotalHours(vector<int>&nums,int hourly){
-        long long totalH=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            totalH+=ceil((double)nums[i]/(double)hourly);
-        }
-        return totalH;
-    }
 public:
-int minEatingSpeed(vector<int> nums, int h) {
-    int low=1,high=findMax(nums);
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        long long totalH=calculateTotalHours(nums,mid);
-        if(totalH<=h){
-            high=mid-1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long hr=0;
+            for(int i:piles){
+                hr+=(i+mid-1)/mid;
+            }
+            if(hr<=h){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        else{
-            low=mid+1;
-        }
-    }
-    return low;
+        return low;
     }
 };
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() { 
-    struct ___ { 
-        static void _() { 
-            std::ofstream("display_runtime.txt") << 0 << '\n'; 
-        } 
-    }; 
-    std::atexit(&___::_); 
-    return 0; 
-}();
-#endif
-
