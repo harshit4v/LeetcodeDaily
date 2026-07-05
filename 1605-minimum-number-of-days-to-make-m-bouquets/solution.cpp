7419ba6f3e758f9f6482de4b71b569045305e259
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-05 12:14:41 +0000

    [LeetCode Sync] Runtime - 26 ms (33.77%), Memory - 70.5 MB (78.50%)

--- 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,44 +1,38 @@
 class Solution {
-    private:
-    bool possible(vector<int>&nums,int days,int m,int k){
-        int n=nums.size();
-        int cnt=0;
-        int ndays=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]<=days){
-                cnt++;
+public:
+    bool possible(vector<int>&bloomday,int m,int k,int day){
+        int flower=0,bouquet=0;
+        for(int i=0;i<bloomday.size();i++){
+            if(bloomday[i]<=day){
+                flower++;
             }
             else{
-                ndays+=(cnt/k);
-                cnt=0;
+                flower=0;
+            }
+            if(flower==k){
+                bouquet++;
+                flower=0;
             }
         }
-        ndays+=(cnt/k);
-        return ndays>=m;
+        return bouquet>=m;
     }
-public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        long long val=m*1LL*k*1LL;
-        int n=bloomDay.size();
-        if(val>n)return -1;
-        int mini=INT_MAX;
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            mini=min(mini,bloomDay[i]);
-            maxi=max(maxi,bloomDay[i]);
+    int minDays(vector<int>& bloomday, int m, int k) {
+        int mini=*min_element(bloomday.begin(),bloomday.end());
+        int maxi=*max_element(bloomday.begin(),bloomday.end());
+        int low=mini,high=maxi;
+        long long need=1LL*m*k;
+        if(need>bloomday.size()){
+            return -1;
         }
-        int low=mini,high=maxi,ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(possible(bloomDay,mid,m,k)){
-                ans=mid;
+            if(possible(bloomday,m,k,mid)){
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
-auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});
