47bbceba2db94a99423afb6b21e61ef7e858fdc2
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-12 11:13:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14 MB (64.49%)

--- 0040-combination-sum-ii/solution.cpp
+++ 0040-combination-sum-ii/solution.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    private:
-    void func(int i,int sum,vector<int>&v,vector<int>&v2,vector<vector<int>>&ans){
+public:
+    void func(int ind,int sum,vector<int>&nums,vector<int>&arr,vector<vector<int>>&result){
         if(sum==0){
-            ans.push_back(v2);
+            result.push_back(arr);
             return;
         }
-        if(sum<0 || i==v.size()){
+        if(sum<0 || ind==nums.size()){
             return;
         }
-        v2.push_back(v[i]);
-        func(i+1,sum-v[i],v,v2,ans);
-        v2.pop_back();
-        for(int j=i+1;j<v.size();j++){
-            if(v[j]!=v[i]){
-                func(j,sum,v,v2,ans);
+        arr.push_back(nums[ind]);
+        func(ind+1,sum-nums[ind],nums,arr,result);
+        arr.pop_back();
+        for(int j=ind+1;j<nums.size();j++){
+            if(nums[j]!=nums[ind]){
+                func(j,sum,nums,arr,result);
                 break;
             }
         }
     }
-public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int>v;
-        vector<vector<int>>ans;
-        sort(candidates.begin(),candidates.end());
-        func(0,target,candidates,v,ans);
-        return ans;
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        vector<int>arr;
+        vector<vector<int>>result;
+        sort(nums.begin(),nums.end());
+        func(0,target,nums,arr,result);
+        return result;
     }
 };
