268bd66fda9825cd24ce291c12f2e4817069dc58
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-12 11:38:15 +0000

    [LeetCode Sync] Runtime - 3 ms (27.73%), Memory - 9.8 MB (82.41%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
-    private:
-    void func(int ind,int n,vector<int>&nums,vector<int>&arr,vector<vector<int>>&ans){
+public:
+    void func(int ind,int n,vector<int>&nums,vector<int>&arr,vector<vector<int>>&result){
         if(ind==n){
-            ans.push_back(arr);
+            result.push_back(arr);
             return;
         }
-        func(ind+1,n,nums,arr,ans);
         arr.push_back(nums[ind]);
-        func(ind+1,n,nums,arr,ans);
+        func(ind+1,n,nums,arr,result);
         arr.pop_back();
+        func(ind+1,n,nums,arr,result);
     }
-public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>arr;
-        vector<vector<int>>ans;
-        func(0,nums.size(),nums,arr,ans);
-        return ans;
+        vector<vector<int>>result;
+        func(0,nums.size(),nums,arr,result);
+        return result;
     }
 };
