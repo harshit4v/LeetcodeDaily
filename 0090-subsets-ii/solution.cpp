b796de3d7e420b1a73d02f32df8ebaf2e51ae46e
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-13 14:33:28 +0000

    [LeetCode Sync] Runtime - 2 ms (40.16%), Memory - 10.4 MB (85.06%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -1,28 +1,24 @@
 class Solution {
-    private:
-    void func(int ind,vector<int>&v,vector<int>&v2,vector<vector<int>>&ans){
-        if(ind==v2.size()){
-            ans.push_back(v);
+public:
+    void func(int ind,vector<int>&arr,vector<int>&nums,vector<vector<int>>&ans){
+        if(ind==nums.size()){
+            ans.push_back(arr);
             return;
         }
-        v.push_back(v2[ind]);
-        func(ind+1,v,v2,ans);
-        v.pop_back();
-        for(int j=ind+1;j<v2.size();j++){
-            if(v2[j]!=v2[ind]){
-                func(j,v,v2,ans);
-                return;
-            }
+        arr.push_back(nums[ind]);
+        func(ind+1,arr,nums,ans);
+        arr.pop_back();
+        int j=ind+1;
+        while(j<nums.size() && nums[ind]==nums[j]){
+            j++;
         }
-        func(v2.size(),v,v2,ans);
+        func(j,arr,nums,ans);
     }
-public:
-    vector<vector<int> > subsetsWithDup(vector<int>& nums) {
-        //your code goes here
-        vector<vector<int>>ans; 
-        vector<int>v;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>arr;
+        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        func(0,v,nums,ans);
+        func(0,arr,nums,ans);
         return ans;
     }
 };
