dd84cb5b8b4a8f0259d2464afc3c9b32288b665f
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-28 06:12:24 +0530

    Time: 0 ms (100%), Space: 12.5 MB (47.83%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -13,44 +13,44 @@ class Solution {
 public:
 
     //o(n^2) soln 
-    // bool isonepresent(TreeNode *node){
-    //     if(node==NULL){
-    //         return NULL;
-    //     }
-    //     if(node->val==1){
-    //         return true;
-    //     }
-    //     return isonepresent(node->left)||isonepresent(node->right);
-    // }
-    // TreeNode* pruneTree(TreeNode* root) {
-    //     if(root==NULL){
-    //         return NULL;
-    //     }
-    //     if(!isonepresent(root->left)){
-    //         root->left=NULL;
-    //     }
-    //     if(!isonepresent(root->right)){
-    //         root->right=NULL;
-    //     }
-    //     pruneTree(root->left);
-    //     pruneTree(root->right);
-    //     if(root->left!=NULL && root->right!=NULL && root->val==0){
-    //         return NULL;
-    //     }
-    //     return root;
-    // }
-
-
-    //o(n) soln
+    bool isonepresent(TreeNode *node){
+        if(node==NULL){
+            return false;
+        }
+        if(node->val==1){
+            return true;
+        }
+        return isonepresent(node->left)||isonepresent(node->right);
+    }
     TreeNode* pruneTree(TreeNode* root) {
         if(root==NULL){
             return NULL;
         }
-        root->left=pruneTree(root->left);
-        root->right=pruneTree(root->right);
+        if(!isonepresent(root->left)){
+            root->left=NULL;
+        }
+        if(!isonepresent(root->right)){
+            root->right=NULL;
+        }
+        pruneTree(root->left);
+        pruneTree(root->right);
         if(root->left==NULL && root->right==NULL && root->val==0){
             return NULL;
         }
         return root;
     }
+
+
+    //o(n) soln
+    // TreeNode* pruneTree(TreeNode* root) {
+    //     if(root==NULL){
+    //         return NULL;
+    //     }
+    //     root->left=pruneTree(root->left);
+    //     root->right=pruneTree(root->right);
+    //     if(root->left==NULL && root->right==NULL && root->val==0){
+    //         return NULL;
+    //     }
+    //     return root;
+    // }
 };
