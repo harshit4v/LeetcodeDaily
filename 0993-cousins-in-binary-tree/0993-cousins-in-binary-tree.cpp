af964dcdd34b219e0c0a39379060febc23300355
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-07-30 13:44:09 +0530

    Time: 0 ms (100%), Space: 14 MB (48.58%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -6,42 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int levelx=-1,levely=-1;
-    TreeNode *parentx=NULL,*parenty=NULL;
-    void dfs(TreeNode *root,TreeNode *parent,int level,int x,int y){
-        if(root==NULL){
-            return ;
+    int levelx = -1, levely = -1;
+    TreeNode *parentx = NULL, *parenty = NULL;
+    void dfs(TreeNode* root, TreeNode* parent, int level, int x, int y) {
+        if (root == NULL) {
+            return;
         }
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                TreeNode *node=q.front();
-                q.pop();
-                if(root->val==x){
-                    levelx=level;
-                    parentx=parent;
-                }
-                if(root->val==y){
-                    levely=level;
-                    parenty=parent;
-                }
-                dfs(root->left,root,level+1,x,y);
-                dfs(root->right,root,level+1,x,y);
-            }
+        if (root->val == x) {
+            levelx = level;
+            parentx = parent;
         }
+        if (root->val == y) {
+            levely = level;
+            parenty = parent;
+        }
+        dfs(root->left, root, level + 1, x, y);
+        dfs(root->right, root, level + 1, x, y);
     }
-    bool isCousins(TreeNode* root, int x, int y) {
-        if(root==NULL){
-            return NULL;
-        }
-        dfs(root,NULL,0,x,y);
-        return (levelx==levely && parentx!=parenty);
+bool isCousins(TreeNode* root, int x, int y) {
+    if (root == NULL) {
+        return false;
     }
-};
+    dfs(root, NULL, 0, x, y);
+    return (levelx == levely && parentx != parenty);
+}
+}
+;
