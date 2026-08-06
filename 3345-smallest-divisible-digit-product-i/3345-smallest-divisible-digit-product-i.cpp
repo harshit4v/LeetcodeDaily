b321d4c498ecbd0a37337fc620a173671b7ee900
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2026-08-06 21:48:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (73.4%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        while(true){
+            int pro=1;
+            int x=n;
+            while(x>0){
+                pro*=(x%10);
+                x/=10;
+            }
+            if(pro%t==0){
+                return n;
+            }
+            n++;
+        }
+
+    }
+};
